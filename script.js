38fb7f70a6cd1cb66be2721bc34ac201b36d455f
timememe <113831656+timememe@users.noreply.github.com>
2024-09-05 18:06:08 +0500

    5

--- script.js
+++ script.js
@@ -12,4 +12,42 @@ $(document).ready(function() {
             }
         });
     });
+
+    // Плавные переходы по якорным ссылкам
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000); // Скорость перехода
+        }
+        $('.nav-links').removeClass('nav-active'); // Закрытие меню после клика
+        $('.burger').removeClass('toggle'); // Возвращение бургера в исходное состояние
+    });
 });
+
+$(document).ready(function() {
+    // Анимация при загрузке страницы
+    setTimeout(function() {
+        $('.hero, .claim, .mask-group-icon').addClass('visible'); // Сразу делаем видимыми
+    }, 100); // Небольшая задержка
+
+    // Анимация при скролле для остальных элементов
+    function checkVisibility() {
+        $('.fade-in').each(function() {
+            var elementTop = $(this).offset().top;
+            var viewportBottom = $(window).scrollTop() + $(window).height();
+
+            if (elementTop < viewportBottom - 50) { // Добавляем небольшой оффсет для плавности
+                $(this).addClass('visible');
+            }
+        });
+    }
+
+    // Запуск проверки при скролле
+    $(window).on('scroll', checkVisibility);
+    checkVisibility(); // Первичная проверка при загрузке
+});
+
+
